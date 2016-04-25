53f6a42614686e0be0152031859836c5c6163e1e
Andreas <Andreas>
2016-04-26 00:31:01 +0200

    add Bourdoncle widening / eval sequence algorithm

--- src/interpreter.py
+++ src/interpreter.py
@@ -8,8 +8,6 @@
 ##############################
 
 
-import boxes
-
 # (I) Arithmetic expressions
 # 
 # CONST
@@ -23,7 +21,6 @@ import boxes
 # X + Y ==> ['+', X, Y]
 #
 # No nested expressions supported yet.
-
 
 #
 # (II) Assignments
@@ -45,7 +42,7 @@ import boxes
 # ['<=', 'x', 'y']
 
 
-class Method_CFG():
+class MethodCFG():
 
     def __init__(self, init_location, end_location):
         self.control_locs = [init_location, end_location]
@@ -53,10 +50,12 @@ class Method_CFG():
         self.end_loc = end_location
         self.widen_points = []
         self.edges = {}
-
+        self.out_edges = {}
+        self.in_edges = {}
+        self.counter = 0
+        
     def to_string(self):
         result = ''
-        
         for (s, t) in self.edges:
             (cond, actions) = self.edges[(s, t)]
             result += '%s -> %s \n\t' % (s, t)
@@ -76,9 +75,67 @@ class Method_CFG():
         self.control_locs.append(control_loc)
         if is_widen_point:
             self.widen_points.append(control_loc)
-
+        self.out_edges[control_loc] = []
+        self.in_edges[control_loc] = []
+            
     def set_edge(self, loc1, loc2, condition, assignments):
-        self.edges[(loc1, loc2)] = [condition, assignments]
+        edge_value = [condition, assignments]
+        assert loc1 in self.control_locs
+        assert loc2 in self.control_locs
+        self.edges[(loc1, loc2)] = edge_value
+        self.out_edges.setdefault(loc1, []).append(loc2)
+        self.in_edges.setdefault(loc2, []).append(loc1)
+       
+    def compute_bourdoncle_widenpoints(self, reverse=False):
+        dfn = {}
+        partition = []
+        self.counter = 0
+        stack = []
+        for node in self.control_locs:
+            dfn[node] = 0
+            
+        def component(vertex):
+            p = []
+            outgoings = (self.out_edges[vertex]
+                         if not reverse else self.in_edges[vertex])
+            for succ in outgoings:
+                if dfn[succ] == 0:
+                    visit(succ, p)
+            return [vertex] + p
+            
+        def visit(loc, p):
+            stack.append(loc)
+            self.counter += 1
+            head = self.counter
+            dfn[loc] = self.counter
+            loop = False
+            outgoings = (self.out_edges[loc]
+                         if not reverse else self.in_edges[loc])
+            for succ in outgoings:
+                if dfn[succ] == 0:
+                    min = visit(succ, p)
+                else:
+                    min = dfn[succ]
+                if min <= head and min != -1:
+                    head = min
+                    loop = True
+            if head == dfn[loc]:
+                dfn[loc] = -1
+                # pop
+                element = stack.pop()
+                if loop:
+                    while element != loc:
+                        dfn[element] = 0
+                        element = stack.pop()
+                    p.insert(0, component(loc))
+                else:
+                    p.insert(0, loc)
+            return head
+        if reverse:
+            visit(self.end_loc, partition)
+        else:
+            visit(self.init_loc, partition)
+        return partition
         
     def forward_analyze(self,
                         dom,
@@ -97,8 +154,9 @@ class Method_CFG():
                 return dom.op_binary(value,
                                      '+',
                                      variable,
-                                     expression[0],
+                                     op1,
                                      0)
+            
             elif len(expression) == 3:
                 (operator, op1, op2) = expression
                 return dom.op_binary(value,
@@ -127,20 +185,15 @@ class Method_CFG():
                 buffer = values[loc]
                 for (s, t) in self.edges:
                     if t == loc:
-                        # print "EDGE: %s -> %s" %  (s, t)
                         (condition, assignments) = self.edges[(s, t)]
                         # is there a condition?
                         inflow = (apply_condition(condition, values[s])
                                   if (condition is not None) else values[s])
-                        
                         for assignment in assignments:
                             inflow = apply_assignment(assignment, inflow)
-                            
                         buffer = dom.union(buffer, inflow)
+                        
                 new_values[loc] = buffer
-            #print "PRE WIDEN:"
-            #for loc in self.control_locs:
-            #    print('"%s" : "%s"' % (loc, dom.to_string(new_values[loc])))
             if do_widen:
                 for loc in self.widen_points:
                     new_values[loc] = dom.widen(values[loc], new_values[loc])
@@ -151,14 +204,6 @@ class Method_CFG():
                 smaller_or_equal = dom.is_subseteq(new_values[loc], values[loc])
                 is_increasing = larger_or_equal and not smaller_or_equal
                 postfixpoint_reached = postfixpoint_reached and not is_increasing
-            #print "POST WIDEN:"
-            #for loc in self.control_locs:
-            #    print('"%s" : "%s"' % (loc, dom.to_string(new_values[loc])))
-            
-            if postfixpoint_reached:
-                for loc in self.control_locs:
-                    print('"%s" : "%s"' % (loc, dom.to_string(new_values[loc])))
-
             return (postfixpoint_reached, new_values)
 
         postfixpoint_reached = False
@@ -169,4 +214,9 @@ class Method_CFG():
                 = iterate(values,
                           iterations > iterations_without_widening)
             iterations += 1
+        print "ITERATIONS: %s" % iterations
+        print "**************************"
+        for loc in values:
+            print('"%s" : "%s"' % (loc, dom.to_string(values[loc])))
+            print "**************************"
             
